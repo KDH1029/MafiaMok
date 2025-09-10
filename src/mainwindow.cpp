8df5333cf932a4a7bac9b5564ca4fb83ed6c8e45
Song-yeonu <yeonu0070@kw.ac.kr>
2025-09-10 17:19:36 +0900

    Update mainwindow.cpp

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -114,6 +114,7 @@ void MainWindow::placeStone(int row, int col, int value)
     {
         qDebug() << "P1 win and P2 Lose!"; // 플레이어1 승리조건
         win_event = true;
+        End_event(win_event);
     }
 }
 
@@ -189,6 +190,7 @@ void MainWindow::onGraphicsViewClicked(QPointF pos)
                 {
                     qDebug() << "You Lose!"; // 사용자 패배 조건(목숨이 깎이는 경우는 돌을 잘못 지우는 경우밖에 없으므로)
                     win_event = true;
+                    End_event(win_event);
                 }
             }
             this->udp->send(QString("%1,%2,0").arg(cell.x()).arg(cell.y()));
