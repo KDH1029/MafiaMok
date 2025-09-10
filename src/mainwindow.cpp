4d75367d05e04a04411d7ca858a25a01358d6966
KDH1029 <dohappy1029@gmail.com>
2025-09-10 13:04:10 +0900

    .

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -59,21 +59,28 @@ MainWindow::~MainWindow()
 
 void MainWindow::handlePoint(Point p)
 {
-    qDebug()<<p.x;
-    qDebug()<<p.y;
+    qDebug() << p.x;
+    qDebug() << p.y;
 
-    if (!this->field->turn){
-        if(this->field->place(p.x, p.y, p.value)){
-            if(this->field->check()){
-                //win
+    if (!this->field->turn)
+    {
+        if (this->field->place(p.x, p.y, p.value))
+        {
+            if (this->field->check())
+            {
+                // win
             }
-            //보드 그리기
-        }else {
-            //err: 잘못된 위치
+            // 보드 그리기
+        }
+        else
+        {
+            // err: 잘못된 위치
         };
         this->field->turn = true;
-    }else{
-        //err: 턴 오류
+    }
+    else
+    {
+        // err: 턴 오류
     }
 }
 
@@ -82,15 +89,15 @@ void MainWindow::drawBoard()
     scene->clear();
     for (int i = 0; i < boardSize; i++)
     {
-        scene->addLine(0, i* cellSize, (boardSize - 1) * cellSize, i * cellSize); // 가로 //격자를 살짝 내리는 방향으로 .
+        scene->addLine(0, i * cellSize, (boardSize - 1) * cellSize, i * cellSize); // 가로 //격자를 살짝 내리는 방향으로 .
         scene->addLine(i * cellSize, 0, i * cellSize, (boardSize - 1) * cellSize); // 세로
     }
 }
 
 QPointF MainWindow::mapToCell(const QPointF &pos)
 {
-    int col = int(pos.x()) / cellSize;
-    int row = int(pos.y()) / cellSize;
+    int col = (int)(double(pos.x()) / cellSize + 0.5);
+    int row = (int)(double(pos.y()) / cellSize + 0.5);
     return QPointF(row, col); // ok
 }
 
@@ -154,12 +161,12 @@ void MainWindow::showMafiaEffect(int row, int col, int stoneSize)
 void MainWindow::onGraphicsViewClicked(QPointF pos)
 {                                  // 여기서 발생한 모든 이벤트는 상대에게도 전송. 전송한 이벤트는 상대의 수신 함수에서 처리
     QPointF cell = mapToCell(pos); // clicked-->포인트 가져오기
-    if (playchoice==0)
+    if (playchoice == 0)
     {                                           // 돌 놓기 선택
         placeStone(cell.x(), cell.y(), player); // player마다 다른 돌
     }
 
-    else if (playchoice==1)
+    else if (playchoice == 1)
     { // 돌 제거 선택
         if (board[cell.x()][cell.y()] != player)
         {
@@ -176,13 +183,13 @@ void MainWindow::onGraphicsViewClicked(QPointF pos)
             else
             {
                 player_life--;
-                cout<<"선량한 시민 돌이 사망했습니다..."<<endl;
-                //addBubble("선량한 시민 돌이 사망했습니다...");
+                cout << "선량한 시민 돌이 사망했습니다..." << endl;
+                // addBubble("선량한 시민 돌이 사망했습니다...");
             }
         }
     }
 
-    else if (playchoice==2)
+    else if (playchoice == 2)
     { // 돌 회유 선택
         if (board[cell.x()][cell.y()] != 2)
         {
@@ -201,7 +208,6 @@ void MainWindow::onGraphicsViewClicked(QPointF pos)
         ui->label->setText("Choose One."); // 선택이 안됐거나 예외상황일시.
         return;
     }
-
 }
 
 bool MainWindow::eventFilter(QObject *obj, QEvent *event)
@@ -219,7 +225,6 @@ bool MainWindow::eventFilter(QObject *obj, QEvent *event)
 void MainWindow::on_radioButton_clicked()
 {
     playchoice = 0; // 돌 놓기
-
 }
 
 void MainWindow::on_radioButton_2_clicked()
